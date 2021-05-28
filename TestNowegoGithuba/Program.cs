f2673d58f28749a81fd49084021eb21ba55b4e26
Daniel Huber <dannyyhub@gmail.com>
2021-05-28 22:05:37 +0200

    TestNowegoGithuba

---
+++ TestNowegoGithuba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestNowegoGithuba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Działa?");
+        }
+    }
+}
